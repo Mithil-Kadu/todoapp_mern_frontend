62d090e5dc3e7bf7acb3e1a85ca75d4a55408c81
Mithil Kadu <128603495+Mithil-Kadu@users.noreply.github.com>
2025-01-13 10:13:02 +0530

    Updated baseUrl in HandleApi.js

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://todoapp-mern-backend-6jf8.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
